87363851f79eee1b1466c12c9b87046694354aa7
adrian.frimpong <adrian.frimpong@gmail.com>
2014-06-13 22:40:55 -0400

    git init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('comedyhackApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
